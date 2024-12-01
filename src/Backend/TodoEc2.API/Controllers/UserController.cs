2c3788dbbf1b3a7d3850b0351a67f9ccd6a485fc
Pedro Lima <tsc.pedrolima@gmail.com>
2024-12-01 09:05:02 -0300

    feat: add endpoint to register an user

---
+++ src/Backend/TodoEc2.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoEc2.Communication.Requests;
+using TodoEc2.Communication.Responses;
+
+namespace TodoEc2.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/Shared/TodoEc2.Communication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,7 @@
+﻿namespace TodoEc2.Communication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
