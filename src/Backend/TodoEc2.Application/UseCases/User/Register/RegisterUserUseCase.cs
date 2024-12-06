eaad2a43fb283fcbdbf0724c3fcd715b289dc93e
Pedro Lima <tsc.pedrolima@gmail.com>
2024-12-06 15:11:24 -0300

    feat: map user entity using AutoMapper

--- src/Backend/TodoEc2.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/TodoEc2.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,4 +1,5 @@
-﻿using TodoEc2.Communication.Requests;
+﻿using TodoEc2.Application.Services.AutoMapper;
+using TodoEc2.Communication.Requests;
 using TodoEc2.Communication.Responses;
 using TodoEc2.Exceptions.ExceptionBase;
 
@@ -11,7 +12,12 @@ namespace TodoEc2.Application.UseCases.User.Register
             // Validade
             Validate(request);
             // Mapping
-            
+            var autoMapper = new AutoMapper.MapperConfiguration(options =>
+            {
+                options.AddProfile(new AutoMapping());
+            }).CreateMapper();
+
+            var user = autoMapper.Map<Domain.Entities.User>(request);
             // Criptografy
 
             // Save DB
