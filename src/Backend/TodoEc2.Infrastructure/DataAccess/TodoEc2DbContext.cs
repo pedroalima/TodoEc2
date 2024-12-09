acdab131905d6854ebc8475d3a9273142208ea04
Pedro Lima <tsc.pedrolima@gmail.com>
2024-12-08 21:15:42 -0300

    feat: add database context with entity framework core

---
+++ src/Backend/TodoEc2.Infrastructure/DataAccess/TodoEc2DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoEc2.Domain.Entities;
+
+namespace TodoEc2.Infrastructure.DataAccess
+{
+    internal class TodoEc2DbContext: DbContext
+    {
+        public TodoEc2DbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoEc2DbContext).Assembly);
+        }
+    }
+}
