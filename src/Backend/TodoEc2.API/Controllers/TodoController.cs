d50af0f50be9d17fbb8f360690c30978d6c8c272
Pedro Lima <tsc.pedrolima@gmail.com>
2024-12-17 17:14:46 -0300

    fix: change method to async

--- src/Backend/TodoEc2.API/Controllers/TodoController.cs
+++ src/Backend/TodoEc2.API/Controllers/TodoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TodoEc2.Application.UseCases.Todo.Create;
+using TodoEc2.Communication.Responses;
 
 namespace TodoEc2.API.Controllers
 {
@@ -8,11 +9,12 @@ namespace TodoEc2.API.Controllers
     public class TodoController : ControllerBase
     {
         [HttpPost]
-        public IActionResult Create(
+        [ProducesResponseType(typeof(ResponseCreateTodoJson), StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(
             [FromServices] ICreateTodoUseCase useCase,
             [FromBody] RequestCreateTodoJson request)
         {
-            var result = useCase.Execute(request);
+            var result = await useCase.Execute(request);
 
             return Created(string.Empty, result);
         }
--- src/Backend/TodoEc2.Infrastructure/DataAccess/TodoEc2DbContext.cs
+++ src/Backend/TodoEc2.Infrastructure/DataAccess/TodoEc2DbContext.cs
@@ -9,6 +9,8 @@ namespace TodoEc2.Infrastructure.DataAccess
 
         public DbSet<User> Users { get; set; }
 
+        public DbSet<Todo> Todos { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoEc2DbContext).Assembly);
