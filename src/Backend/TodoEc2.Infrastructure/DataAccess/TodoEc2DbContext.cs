0b788b1bffcbdd62aa22a3e6241f20b4a1f0071d
Pedro Lima <tsc.pedrolima@gmail.com>
2024-12-24 11:37:56 -0300

    fix: convert tinyInt to bool type

--- src/Backend/TodoEc2.Infrastructure/DataAccess/TodoEc2DbContext.cs
+++ src/Backend/TodoEc2.Infrastructure/DataAccess/TodoEc2DbContext.cs
@@ -14,6 +14,15 @@ namespace TodoEc2.Infrastructure.DataAccess
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoEc2DbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .Property(e => e.Active)
+                .HasConversion(
+                    v => v ? (byte)1 : (byte)0, // Converte de bool para TINYINT ao salvar
+                    v => v == 1                // Converte de TINYINT para bool ao ler
+                );
         }
     }
 }
