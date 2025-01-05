f10fe73bfaecb946a95f3f90b8c569155c19e07c
Pedro Lima <tsc.pedrolima@gmail.com>
2025-01-04 21:06:48 -0300

    feat: add UserUpdateOnlyRepository interface and validate in UpdateUserUseCase

---
+++ src/Backend/TodoEc2.Application/UseCases/User/Update/UpdateUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using TodoEc2.Communication.Requests;
+
+namespace TodoEc2.Application.UseCases.User.Update
+{
+    public class UpdateUserValidator : AbstractValidator<RequestUpdateUserJson>
+    {
+        public UpdateUserValidator()
+        {
+            RuleFor(request => request.Name).NotEmpty().WithMessage("O campo Nome não pode ser vazio");
+            RuleFor(request => request.Email).NotEmpty().WithMessage("O campo E-mail não pode ser vazio");
+
+            When(request => !string.IsNullOrWhiteSpace(request.Email), () =>
+            {
+                RuleFor(request => request.Email).EmailAddress().WithMessage("Este não é um e-mail válido");
+            });
+        }
+    }
+}
---
+++ src/Backend/TodoEc2.Domain/Repositories/User/IUserUpdateOnlyRepository.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace TodoEc2.Domain.Repositories.User
+{
+    public interface IUserUpdateOnlyRepository
+    {
+        public Task<Entities.User> GetById(long id);
+
+        public void Update(Entities.User user);
+    }
+}
