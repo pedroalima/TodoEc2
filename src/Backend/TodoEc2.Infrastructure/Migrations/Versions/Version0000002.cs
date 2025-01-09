13b731d6c17d19d33d9eef56f8a8f7c155a2228f
Pedro Lima <tsc.pedrolima@gmail.com>
2025-01-09 14:05:45 -0300

    feat: add todo table migration

---
+++ src/Backend/TodoEc2.Infrastructure/Migrations/Versions/Version0000002.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace TodoEc2.Infrastructure.Migrations.Versions
+{
+    [Migration(DatabaseVersions.TABLE_TODO, "Create table to save the task's information")]
+    public class Version0000002 : VersionBase
+    {
+        public override void Up()
+        {
+            CreateTable("Todos")
+                .WithColumn("Title").AsString(255).NotNullable()
+                .WithColumn("Description").AsString(2000).Nullable()
+                .WithColumn("Status").AsInt32().NotNullable()
+                .WithColumn("UserId").AsInt64().NotNullable().ForeignKey("FK_Todo_User_Id", "Users", "Id");
+        }
+    }
+}
