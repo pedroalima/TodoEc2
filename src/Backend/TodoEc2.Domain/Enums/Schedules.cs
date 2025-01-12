b1001643a60babde3aaaef910d7ecbbd79703e6b
Pedro Lima <tsc.pedrolima@gmail.com>
2025-01-11 21:40:34 -0300

    feat: add schedule enum

---
+++ src/Backend/TodoEc2.Domain/Enums/Schedules.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoEc2.Domain.Enums
+{
+    public enum Schedules
+    {
+        _00_00 = 1,
+        _00_30 = 2,
+        _01_00 = 3,
+        _01_30 = 4,
+        _02_00 = 5,
+        _02_30 = 6,
+        _03_00 = 7,
+        _03_30 = 8,
+        _04_00 = 9,
+        _04_30 = 10,
+        _05_00 = 11,
+        _05_30 = 12,
+        _06_00 = 13,
+        _06_30 = 14,
+        _07_00 = 15,
+        _07_30 = 16,
+        _08_00 = 17,
+        _08_30 = 18,
+        _09_00 = 19,
+        _09_30 = 20,
+        _10_00 = 21,
+        _10_30 = 22,
+        _11_00 = 23,
+        _11_30 = 24,
+        _12_00 = 25,
+        _12_30 = 26,
+    }
+}
